9c85ca1d47f8e296589074488bb97e4da0c6e73d
Alan Roddick <aroddick16@gmail.com>
2021-10-14 23:25:00 -0700

    Update to use functional components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,43 +2,38 @@ import { Component } from "react";
 import "./App.css";
 
 import MessageList from "./Components/MessageList/MessageList";
+import UserList from "./Components/UserList/UserList";
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-
+const App = () => {
     const server = new EventSource("http://localhost:3001/stream/a");
     server.addEventListener("message", (event) => {
-      if (event.data === "Goodbye!") {
-        console.log("Closing SSE connection");
-        server.close();
-      } else {
-        console.log(event.data);
-      }
+        if (event.data === "Goodbye!") {
+            console.log("Closing SSE connection");
+            server.close();
+        } else {
+            console.log(event.data);
+        }
     });
     server.onerror = (_event) => {
-      console.log("Connection lost, reestablishing");
+        console.log("Connection lost, reestablishing");
     };
-  }
 
-  render() {
     return (
-      <div className="chatServer">
-        <div className="messages">
-          <div className="messagesWrapper">
-            <div className="chatBoxTop">
+        <div className="chatServer">
+            <div className="messages">
+                <div className="messagesWrapper">
+                    <div className="chatBoxTop">
 
+                    </div>
+                    <div className="chatBoxBottom">
+                        <MessageList />
+                    </div>
+                </div>
             </div>
-            <div className="chatBoxBottom">
-              <MessageList />
+            <div className="onlineUsers">
+                <UserList />
             </div>
-          </div>
         </div>
-        <div className="onlineUsers">
-
-        </div>
-      </div>
     );
-  }
 }
 export default App;
